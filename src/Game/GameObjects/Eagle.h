6fb4c8f0259159f6e7950790dbfe0eeea61109c3
mikhail-vl-ryazanov <mikhail.vl.ryazanov@yandex.ru>
2026-01-02 17:09:32 +0300

    Added DEPTH BUFFER

--- src/Game/GameObjects/Eagle.h
+++ src/Game/GameObjects/Eagle.h
@@ -15,7 +15,7 @@ public:
         Alive = 0,
         Dead
     };
-    Eagle(const glm::vec2& position, const glm::vec2& size, const float rotation);
+    Eagle(const glm::vec2& position, const glm::vec2& size, const float rotation, const float layer);
     virtual void render() const override;
     void update(const uint64_t delta) override;
 
--- src/Renderer/ShaderProgram.h
+++ src/Renderer/ShaderProgram.h
@@ -11,6 +11,7 @@ namespace RenderEngine {
 		bool isCompiled() const { return m_isCompiled;  }
 		void use() const;
 		void setInt(const std::string& name, const GLint value);
+		void ShaderProgram::setFloat(const std::string& name, const GLfloat value);
 		void setMatrix4(const std::string& name, const glm::mat4& matrix);
 
 		ShaderProgram() = delete;
