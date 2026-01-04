d4bb2444310a4b151a9beedfdacd15002b0cb833
mikhail-vl-ryazanov <mikhail.vl.ryazanov@yandex.ru>
2026-01-04 22:23:24 +0300

    Added types of tanks

--- src/Game/GameObjects/Tank.h
+++ src/Game/GameObjects/Tank.h
@@ -2,7 +2,7 @@
 
 #include <glm/vec2.hpp>
 #include <memory>
-
+#include <string>
 #include "IGameObject.h"
 #include "../../Renderer/SpriteAnimator.h"
 #include "../../System/Timer.h"
@@ -10,19 +10,76 @@
 namespace RenderEngine {
 	class Sprite;
 }
+
 class Bullet;
 
 class Tank : public IGameObject {
 public:
 
-	enum class EOrientation {
+    enum class ETankType : uint8_t
+    {
+        Player1Yellow_type1,
+        Player1Yellow_type2,
+        Player1Yellow_type3,
+        Player1Yellow_type4,
+
+        Player2Green_type1,
+        Player2Green_type2,
+        Player2Green_type3,
+        Player2Green_type4,
+
+        EnemyWhite_type1,
+        EnemyWhite_type2,
+        EnemyWhite_type3,
+        EnemyWhite_type4,
+
+        EnemyGreen_type1,
+        EnemyGreen_type2,
+        EnemyGreen_type3,
+        EnemyGreen_type4,
+
+        EnemyRed_type1,
+        EnemyRed_type2,
+        EnemyRed_type3,
+        EnemyRed_type4
+    };
+
+    inline static std::string TankTypeToSpriteString[] = {
+        "player1_yellow_tank_type1_sprite",
+        "player1_yellow_tank_type2_sprite",
+        "player1_yellow_tank_type3_sprite",
+        "player1_yellow_tank_type4_sprite",
+
+        "player2_green_tank_type1_sprite",
+        "player2_green_tank_type2_sprite",
+        "player2_green_tank_type3_sprite",
+        "player2_green_tank_type4_sprite",
+
+        "enemy_white_tank_type1_sprite",
+        "enemy_white_tank_type2_sprite",
+        "enemy_white_tank_type3_sprite",
+        "enemy_white_tank_type4_sprite",
+
+        "enemy_green_tank_type1_sprite",
+        "enemy_green_tank_type2_sprite",
+        "enemy_green_tank_type3_sprite",
+        "enemy_green_tank_type4_sprite",
+
+        "enemy_red_tank_type1_sprite",
+        "enemy_red_tank_type2_sprite",
+        "enemy_red_tank_type3_sprite",
+        "enemy_red_tank_type4_sprite"
+    };
+
+	enum class EOrientation : uint8_t {
 		Top,
 		Bottom,
 		Left,
 		Right
 	};
 
-	Tank(const double maxVelocity,
+    Tank(const Tank::ETankType eType,
+        const double maxVelocity,
 		const glm::vec2& position, 
 		const glm::vec2& size, 
 		const float layer);
@@ -58,4 +115,6 @@ private:
 	double m_maxVelocity;
 	bool m_isSpawning;
 	bool m_hasShield;
+
+    static const std::string& getTankSpriteFromType(const ETankType eType);
 };
